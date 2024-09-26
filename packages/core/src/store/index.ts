fac91604e3132902443989de3e87c65b10cd5266
Isa(yunjongryu) <yunjonglyu@gmail.com>
2024-09-26 21:41:23 +0900

    feat(STORE): BASIC AUTH STATE HOOK

--- packages/core/src/store/index.ts
+++ packages/core/src/store/index.ts
@@ -1,2 +1,14 @@
-import { getState, setState } from './store';
+import { initState, getState, setState } from './store';
 
+export function initStore(prevState: boolean) {
+  initState('isa-auth', prevState || false);
+}
+/**
+ *
+ * @todo make watch render state
+ */
+export function useAuthState() {
+  const auth: boolean = getState('isa-auth');
+  const setAuth = (value: boolean) => setState('isa-auth', value);
+  return [auth, setAuth] as [typeof auth, typeof setAuth];
+}
